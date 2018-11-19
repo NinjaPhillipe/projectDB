825f146bc9cce532febf0b11c463be0150321917
NinjaPhillipe <NinjaPhillipe>
2018-11-19 15:53:00 +0100

    some change

--- algeToSQL.py
+++ algeToSQL.py
@@ -1,10 +1,56 @@
 import ast
+import unittest
 from operation import *
 
 eq = Eq('Country', Cst('Mali'))
-rel = Rel('CC')
-test = Select(eq,rel)
-
 print(str(eq))
+
+rel = Rel('CC')
 print(rel)
-print(test)
+
+############################SELECT###############################
+print("\n SELECT")
+select = Select(eq,rel)
+# print(select)
+select2 = Select(Eq("Money",100),rel)
+# print(select2)
+#ERROR
+select3 = Select(eq,eq)
+print(select3)
+##########################PROJECTION#############################
+print("\n PROJECTION")
+proj = Proj(["Country","Money"],select)
+print(proj)
+
+proj2 = Proj(["Country","Money"],rel)
+print(proj2)
+
+#ERROR
+proj3 = Proj(["Country","Money"],eq)
+print(proj3)
+
+#############################JOIN################################
+print("\n JOIN")
+
+############################RENAME###############################
+print("\n RENAME")
+
+############################UNION################################
+print("\n UNION")
+union = Union(select,select)
+print(union)
+##########################DIFFERENCE#############################
+print("\n DIFFERENCE")
+diff = Diff(select,select)
+print(diff)
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        ############################SELECT###############################
+        self.assertEqual(str(select),"SELECT * FROM CC WHERE Country = \"Mali\"")
+        self.assertEqual(str(select2),"SELECT * FROM CC WHERE Money = 100")
+        ##########################PROJECTION#############################
+        # self.assertEqual(str(),"SELECT Country,Money FROM (SELECT * FROM CC WHERE Country = \"Mali\")")
+
+if __name__ == '__main__':
+    unittest.main()
--- operation.py
+++ operation.py
@@ -1,4 +1,5 @@
 class Cst:
+    type = ""
     """Objet representant une constante"""
     def __init__(self, name):
         # super(, self).__init__()
@@ -9,6 +10,7 @@ class Cst:
         return "\"{}\"".format(self.name)
 class Rel:
     """Objet representant une table"""
+    type = "rel"
     #verifier par rapport a la base de donnée
     def __init__(self, table):
         # super(, self).__init__()
@@ -17,38 +19,77 @@ class Rel:
         return "{}".format(self.table)
 class Eq:
     """Objet representant une egalité"""
+    type = ""
     def __init__(self, col,constante):
         # super(, self).__init__()
         self.col = col
         self.constante = constante
     def __str__(self):
         return "{} = {}".format(self.col,self.constante)
-class Join:
-    """docstring for ."""
-    def __init__(self, arg):
-        # super(, self).__init__()
-        self.arg = arg
-    def __str__(self):
-        return "NotImplemented"
-class Projection:
-    """docstring for ."""
-    def __init__(self, ArrayCol):
-        # super(, self).__init__()
-        self.arrayCol = ArrayCol
-    def __str__(self):
-        return "NotImplemented"
 class Select:
     """docstring for ."""
+    type = "request"
     def __init__(self, eq,rel):
         # super(, self).__init__()
         self.eq = eq
         self.rel = rel
     def __str__(self):
-        return "SELECT * FROM {1} WHERE {0}".format(str(self.eq),str(self.rel))
-class Diff:
+        if(self.rel.type == "rel"):
+            return "SELECT * FROM {1} WHERE {0}".format(str(self.eq),str(self.rel))
+        else:
+            return "ERROR Select"
+class Proj:
     """docstring for ."""
-    def __init__(self, arg):
+    type = "request"
+    def __init__(self, arrayCol,fff):
         # super(, self).__init__()
+        self.arrayCol = arrayCol
+        self.fff = fff
+    def __str__(self):
+        tmp = ""
+        for t in self.arrayCol:
+            if( tmp != ""):
+                tmp+=","
+            tmp+=t
+        if (self.fff.type == "request"):
+            return "SELECT {} FROM ({})".format(str(tmp),str(self.fff))
+        elif(self.fff.type == "rel"):
+            return "SELECT {} FROM {}".format(str(tmp),str(self.fff))
+        else:
+            return """ERROR {1} is not a valid argument \n ----> SELECT {0} FROM {1}""".format(str(tmp), str(self.fff))
+class Join:
+    """docstring for ."""
+    type = "request"
+    def __init__(self, exp1,exp2):
+        # super(, self).__init__()
+        self.exp1 = exp1
+        self.exp2 = exp2
+    def __str__(self):
+        #si colonene en commun faire une intersection
+        #sinon pas de colonne en commun faire union
+        return "NotImplemented"
+class Rename(object):
+    """docstring for Rename."""
+    def __init__(self, arg):
+        # super(Rename, print("\n SELECT")self).__init__()
         self.arg = arg
     def __str__(self):
-        return "NotImplemented"
+        return "RENAME NOTIMPLEMENTED"
+class Union(object):
+    """docstring for Union."""
+    def __init__(self,exp1,exp2):
+        # super(Union, self).__init__()
+        self.exp1 = exp1
+        self.exp2 = exp2
+    def __str__(self):
+        return "{} UNION {}".format(self.exp1,self.exp2)
+
+class Diff:
+    """docstring for ."""
+    type = "request"
+    def __init__(self, exp1,exp2):
+        # super(, self).__init__()
+        self.exp1 = exp1
+        self.exp2 = exp2
+    def __str__(self):
+        return "{} EXCEPT {}".format(self.exp1,self.exp2)
