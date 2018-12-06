522f850fbe9881ce786f1d6eda6262aef9db7e55
NinjaPhillipe <NinjaPhillipe>
2018-12-06 16:34:39 +0100

    get table name + col + type

--- bd.py
+++ bd.py
@@ -1,29 +1,8 @@
 import sqlite3
 from operation import *
 
-#############################get col name#################
 db = sqlite3.connect("my_db.db")
-db.row_factory = sqlite3.Row
-cursor = db.execute('select * from users')
-# instead of cursor.description:
-row = cursor.fetchone()
-names = row.keys()
-print(names)
-##########################################################
 
-# ("users",['id','name','age'],['INTEGER','TEXT','INTEGER'])
+dbSchema = DbSchema("my_db.db")
 
-# dbschema = DBSchema()
-# dbschema.add_table("Countries",["Name","Capital","Population"],["TEXT","TEXT","INTERGER"])
-
-# db.connect("PATH")
-# dbschema = DBSchema(db)
-
-tab = Table("users",['id','name','age'],['INTEGER','TEXT','INTEGER'])
-
-exp = Select(Eq("Population",Cst(100)),Rel("Countries"))
-# exp=Select("tt",Rel("Countries"))
-
-print(exp.valid)
-if(exp.valid == True):
-    print(exp.execute(db))
+dbSchema.getDbschema()
--- operation.py
+++ operation.py
@@ -1,14 +1,34 @@
+import sqlite3
+
 class DbSchema:
     """docstring for DbSchema."""
-    def __init__(self, db):
+    def __init__(self, dbname):
         # super(DbSchema, self).__init__()
-        tables = []
-        # getDbshema(db)
+        self.tables = []
+        self.dbname = dbname
+        self.db = sqlite3.connect(self.dbname)
+    def getDbschema(self):
+        for table in self.getTables():
+            print(self.getColInfo(table))
 
-    def getDbshema(self,db):
-        print("test")
-    # def add_table(self,tableName,colName,colType):
+    def getTables(self):
+        cursor = self.db.cursor()
+        tablesName = []
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        for tuple in cursor.fetchall():
+            if(tuple[0] != "sqlite_sequence"):
+                tablesName.append(tuple[0])
+        return tablesName
 
+    def getColInfo(self,table):
+        print("__________",table,"____________")
+        colName = []
+        colType = []
+        cursor = self.db.cursor()
+        for col in cursor.execute("PRAGMA table_info('annuaire')").fetchall():
+            colName.append(col[1])
+            colType.append(col[2])
+        print(table,colName,colType)
 
 
 class Table:
@@ -27,7 +47,7 @@ class Cst:
     def __init__(self, name):
         # super(, self).__init__()
         self.name = str(name)
-    def execute(self, db):
+    def execute(self, dbSchema):
         if (self.name.isdigit()):
             return "{}".format(self.name)
         return "\"{}\"".format(self.name)
@@ -40,7 +60,7 @@ class Rel:
     def __init__(self, table):
         # super(, self).__init__()
         self.table = table
-    def execute(self,db):
+    def execute(self,dbSchema):
         return "{}".format(self.table)
 class Eq:
     """Objet representant une egalité"""
@@ -50,8 +70,8 @@ class Eq:
         # super(, self).__init__()
         self.col = col
         self.constante = constante
-    def execute(self,db):
-        return "{} = {}".format(self.col,self.constante.execute(db))
+    def execute(self,dbSchema):
+        return "{} = {}".format(self.col,self.constante.execute(dbSchema))
 class Select:
     """docstring for ."""
     type = "request"
@@ -61,9 +81,9 @@ class Select:
         # super(, self).__init__()
         self.eq = eq
         self.rel = rel
-    def execute(self, db):
+    def execute(self, dbSchema):
         if(self.rel.type == "rel" and self.eq.type == "eq"):
-            self.request = "SELECT * FROM {1} WHERE {0}".format(self.eq.execute(db),self.rel.execute(db))
+            self.request = "SELECT * FROM {1} WHERE {0}".format(self.eq.execute(dbSchema),self.rel.execute(dbSchema))
         else:
             self.request = "Error invalid argument type"
             self.valid = False
@@ -77,18 +97,18 @@ class Proj:
         # super(, self).__init__()
         self.arrayCol = arrayCol
         self.fff = fff
-    def execute(self,db):
+    def execute(self,dbSchema):
         tmp = ""
         for t in self.arrayCol:
             if( tmp != ""):
                 tmp+=","
             tmp+=t
         if (self.fff.type == "request"):
-            return "SELECT {} FROM ({})".format(str(tmp),self.fff.execute(db))
+            return "SELECT {} FROM ({})".format(str(tmp),self.fff.execute(dbSchema))
         elif(self.fff.type == "rel"):
-            return "SELECT {} FROM {}".format(str(tmp),self.fff.execute(db))
+            return "SELECT {} FROM {}".format(str(tmp),self.fff.execute(dbSchema))
         else:
-            return """ERROR {1} is not a valid argument \n ----> SELECT {0} FROM {1}""".format(str(tmp), self.fff.execute(db))
+            return """ERROR {1} is not a valid argument \n ----> SELECT {0} FROM {1}""".format(str(tmp), self.fff.execute(dbSchema))
 class Join:
     """docstring for ."""
     type = "join"
@@ -120,12 +140,12 @@ class Union:
         # super(Union, self).__init__()
         self.exp1 = exp1
         self.exp2 = exp2
-    def execute(self,db):
-        # print(self.exp1.execute(db))
-        return "{} UNION {}".format(self.exp1.execute(db),self.exp2.execute(db))
+    def execute(self,dbSchema):
+        # print(self.exp1.execute(dbSchema))
+        return "{} UNION {}".format(self.exp1.execute(dbSchema),self.exp2.execute(dbSchema))
 
     # def __str__(self):
-    #     return "{} UNION {}".format(self.exp1.execute(db).,self.exp2.execute(db))
+    #     return "{} UNION {}".format(self.exp1.execute(dbSchema).,self.exp2.execute(dbSchema))
 
 class Diff:
     """docstring for ."""
@@ -135,5 +155,5 @@ class Diff:
         # super(, self).__init__()
         self.exp1 = exp1
         self.exp2 = exp2
-    def execute(self,db):
-        return "{} EXCEPT {}".format(self.exp1.execute(db),self.exp2.execute(db))
+    def execute(self,dbSchema):
+        return "{} EXCEPT {}".format(self.exp1.execute(dbSchema),self.exp2.execute(dbSchema))
